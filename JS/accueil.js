86394e0ad7c6534debc117fa3d52f992c9d2ebbe
Esthebann <esthebann.volle@mmibordeaux.com>
2025-02-05 17:05:30 +0100

    fix

--- JS/accueil.js
+++ JS/accueil.js
@@ -1,11 +1,26 @@
-document.querySelector(".intro-begin").addEventListener("click", function (event) {
-    event.preventDefault();
-
-    // Déclenche l'animation
-    document.querySelector(".transition-overlay").style.left = "0";
-
-    // Redirige après l'animation
-    setTimeout(() => {
+document.addEventListener("DOMContentLoaded", function() {
+    var splash = document.getElementById('splash');
+    if(sessionStorage.getItem('internalNav') === 'true'){
+      sessionStorage.removeItem('internalNav');
+      splash.style.display = 'none';
+    } else {
+      splash.addEventListener('animationend', function() {
+        splash.parentNode.removeChild(splash);
+      });
+    }
+    document.querySelector(".intro-begin").addEventListener("click", function(event) {
+      event.preventDefault();
+      sessionStorage.setItem('internalNav', 'true');
+      var overlay = document.querySelector(".transition-overlay");
+      overlay.style.left = "0";
+      setTimeout(() => {
         window.location.href = "index.html";
-    }, 800);
-});
+      }, 6000);
+    });
+    document.querySelector(".credit").addEventListener("click", function(event) {
+      event.preventDefault();
+      sessionStorage.setItem('internalNav', 'true');
+      window.location.href = "credits.html";
+    });
+  });
+  
